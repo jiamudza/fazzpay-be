524be715b06d7e6689ac4cdc4f8bb3cbd7ec9368
jiamudza <jiamudza@gmail.com>
2023-09-05 10:26:32 +0700

    change forgot password method

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const route = express()
+const express = require("express");
+const route = express();
 
-const authController = require('../controller/auth.controller')
+const authController = require("../controller/auth.controller");
 
-route.post('/register', authController.register)
-route.post('/login', authController.login)
-route.post('/pin', authController.pinVerify)
-route.patch('/update-pin', authController.updatePin)
-route.patch('/update-password', authController.updatePassword)
-route.get('/forgotpassword', authController.forgotPassword);
+route.post("/register", authController.register);
+route.post("/login", authController.login);
+route.post("/pin", authController.pinVerify);
+route.patch("/update-pin", authController.updatePin);
+route.patch("/update-password", authController.updatePassword);
+route.post("/forgotpassword", authController.forgotPassword);
 
-module.exports = route
+module.exports = route;
