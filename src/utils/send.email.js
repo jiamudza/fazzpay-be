b34f7ea406d3a42355c28b4df697a6317885a0ce
jiamudza <jiamudza@gmail.com>
2023-09-07 13:18:10 +0700

    change email utils to  promise

--- src/utils/send.email.js
+++ src/utils/send.email.js
@@ -1,36 +1,38 @@
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 
-const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: "465",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-        tls: {
-            rejectUnauthorized: false,
+const sendEmail = (subject, message, send_to, sent_from, reply_to) => {
+    return new Promise((resolve, reject) => {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: "465",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+            tls: {
+                rejectUnauthorized: false,
+            }
+        })
+    
+        const options = {
+            from: sent_from,
+            to: send_to,
+            replyTo: reply_to,
+            subject: subject,
+            html: message
         }
-    })
-
-    const options = {
-        from: sent_from,
-        to: send_to,
-        replyTo: reply_to,
-        subject: subject,
-        html: message
-    }
-
-    //sendEmail
-
-    transporter.sendMail(options, function(err, info) {
-        if(err) {
-            console.log(err)
-            return err;
-        } else{ 
-            console.log(info)
-        }
+    
+        //sendEmail
+    
+        transporter.sendMail(options, function(err, info) {
+            if(err) {
+                console.log(err)
+                return reject(err);
+            } else{ 
+                return resolve(info)
+            }
+        })
     })
 }
 
