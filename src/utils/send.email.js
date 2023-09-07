3937af03376c20d1858bcc675a1b0f0e22b80304
jiamudza <jiamudza@gmail.com>
2023-09-07 15:05:02 +0700

    update utils email send

--- src/utils/send.email.js
+++ src/utils/send.email.js
@@ -4,14 +4,11 @@ require('dotenv').config()
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
-        port: "465",
+        port: 465,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
-        tls: {
-            rejectUnauthorized: false,
-        }
     })
 
     const options = {
